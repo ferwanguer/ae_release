687cf41f483b96984a553744f16f565758089f1e
fwanguem <ferwanguer@me.com>
2022-09-01 14:38:12 +0200

    advances in the course

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//console.log("any message")
+let welcome = "PriceE";
+showMessage(welcome);
+
+// if(true) {
+//     console.log('Yes');
+// }
+
+// for (let i = 0; i<3; i++){
+//     console.log(i)
+// }
+
+showMessage(person.age);
---
+++ utils.js
@@ -0,0 +1,9 @@
+function showMessage(message){
+    document.getElementById('mess').textContent = message;
+}
+
+let person = {
+    name: "John",
+    age: 32,
+    partTIme: false
+};
